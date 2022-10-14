8defd7e6f3c2654f06bc1d792ba9b631d9c9b515
jamipouchi <miquelpuigturon@gmail.com>
2022-10-14 19:49:01 +0200

    stop it

---
+++ src/CentralEnergia/Estado.java
@@ -0,0 +1,84 @@
+package CentralEnergia;
+
+import IA.Energia.Centrales;
+import IA.Energia.Clientes;
+
+import static CentralEnergia.Utils.Utils.*;
+
+import CentralEnergia.Generadores.GeneradorEstadoInicial;
+
+public class Estado {
+    private static Centrales centrales;
+    private static Clientes clientes;
+    private int[] asignacionClientes;
+    private double[] distribucionCentrales;
+    private double[] beneficioCentrales;
+
+    Estado(int t1, int t2, int t3, int numClientes, double p1, double p2, double p3, double proporcionGarantizado,
+            GeneradorEstadoInicial generador)
+            throws Exception {
+        clientes = generarClientes(numClientes, p1, p2, p3, proporcionGarantizado);
+        centrales = generarCentrales(t1, t2, t3);
+        generador.creaEstadoInicial(clientes, centrales);
+        asignacionClientes = generador.getAsignacionClientes();
+        distribucionCentrales = generador.getDistribucionCentrales();
+        beneficioCentrales = generador.getBeneficioCentrales();
+    }
+
+    Estado(Estado estadoAntiguo) {
+        this.asignacionClientes = estadoAntiguo.asignacionClientes.clone();
+        this.beneficioCentrales = estadoAntiguo.beneficioCentrales.clone();
+        this.distribucionCentrales = estadoAntiguo.distribucionCentrales.clone();
+    }
+
+    private Centrales generarCentrales(int t1, int t2, int t3) throws Exception {
+        int[] centralesDeCadaTipo = new int[3];
+        centralesDeCadaTipo[0] = t1;
+        centralesDeCadaTipo[1] = t2;
+        centralesDeCadaTipo[2] = t3;
+        return new Centrales(centralesDeCadaTipo, 1267);
+    }
+
+    private Clientes generarClientes(int numClientes, double p1, double p2, double p3, double proporcionGarantizado)
+            throws Exception {
+        double[] clientesDeCadaTipo = new double[3];
+        clientesDeCadaTipo[0] = p1;
+        clientesDeCadaTipo[1] = p2;
+        clientesDeCadaTipo[2] = p3;
+        return new Clientes(numClientes, clientesDeCadaTipo, proporcionGarantizado, 2943875);
+    }
+
+    public int[] getAsignacionClientes() {
+        return asignacionClientes;
+    }
+
+    public double[] getDistribucionCentrales() {
+        return distribucionCentrales;
+    }
+
+    public double[] getBeneficioCentrales() {
+        return beneficioCentrales;
+    }
+
+    public static Clientes getClientes() {
+        return clientes;
+    }
+
+    public static Centrales getCentrales() {
+        return centrales;
+    }
+    
+    public void desassignar(int numClient) {
+      if(asignacionClientes[numClient] != -1){
+        int numCentral = asignacionClientes[numClient]
+        asignacionClientes[numClient] = -1;
+        Cliente c = clientes.get(numClient);
+        Central ce = centrales.get(numCentral);
+
+        double consumClient = produccionNecesariaToClienteFromCentral(c, ce);
+        distribucionCentrales[numCentral] -= consumClient;
+        if(c.getContrato() == Cliente.GARANTIZADO) beneficioCentrales[numCentral] -= c.getConsumo()*VEnergia.getTarifaClienteGarantizada(c.getTipo());
+          else beneficioCentrales[numCentral] -= c.getConsumo()*VEnergia.getTarifaClienteNoGarantizada(c.getTipo());
+      }
+    }
+}
