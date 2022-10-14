0a18ed44d9deed81377a3908cbd9ae6cb81f4d98
Arnau Gracia López <114861208+agraciaLpz@users.noreply.github.com>
2022-10-14 18:16:49 +0200

    Update Sucesores.java

--- src/Sucesores.java
+++ src/Sucesores.java
@@ -13,15 +13,16 @@ public class Sucesores implements SuccessorFunction {
         //Operador desassiganr
         for(int i = 0; i < estatActual.getAsignacionClientes().size(); ++i){
           Estado nouEstat = new Estado(estatActual);
+            if(not estatActual.clientGarantitzat(i)) {
           if(nouEstat.desassignar(i)) sucesores.add(nouEstat); //On i representa el client a desassignar
-          
+            }
         }
 
         //Operador assignar
         for(int i = 0; i < estatActual.getAsignacionClientes().size(); ++i){
             int[] assignacioClients = estatActual.getAsignacionClientes();
             if(assignacioClients[i] == -1){
-              for(int j = 0; j < estatActual.centrales.size(); ++j){
+              for(int j = 0; j < estatActual.getSizeCentrales(); ++j){
                 Estado nouEstat = new Estado(estatActual);
                 if(nouEstat.assignar(i, j)) sucesores.add(nouEstat);//On i representa el client a assignar
               }
@@ -37,13 +38,13 @@ public class Sucesores implements SuccessorFunction {
               for(int j = i; j < estatActual.getAsignacionClientes().size(); ++j){
                 if(assignacioClients[j] != -1 or assignacioClients[i] != -1){  
                     bool esValid = true;
-                    if((estatActual.clientes[i].getContrato() == Cliente.GARANTIZADO or estatActual.clientes[j].getContrato() == Cliente.GARANTIZADO)){
+                    if(estatActual.clientGarantitzat(i) or estatActual.clientGarantitzat(j)){
                         if(assignacioClients[i] == -1 or assignacioClients[j] == -1) esValid = false;
                     }
                  
                     if(esValid){
                       Estado nouEstat = new Estado(estatActual);
-                      nouEstat.swap(i, j); //On i representa el client a assignar
+                      nouEstat.swap(i, j);
                       sucesores.add(nouEstat);
                     }
                 }
