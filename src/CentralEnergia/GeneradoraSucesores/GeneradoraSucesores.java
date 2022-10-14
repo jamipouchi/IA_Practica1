4127a36c20bf61679d77d591c12db8cbe858d309
jamipouchi <miquelpuigturon@gmail.com>
2022-10-14 19:40:55 +0200

    regeneracion binarios

--- src/CentralEnergia/GeneradoraSucesores/GeneradoraSucesores.java
+++ src/CentralEnergia/GeneradoraSucesores/GeneradoraSucesores.java
@@ -1,7 +1,9 @@
 package CentralEnergia.GeneradoraSucesores;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import CentralEnergia.Estado;
 import aima.search.framework.SuccessorFunction;
 
 public class GeneradoraSucesores implements SuccessorFunction {
@@ -9,7 +11,8 @@ public class GeneradoraSucesores implements SuccessorFunction {
     @Override
     public List getSuccessors(Object arg0) {
         // TODO Auto-generated method stub
-        return null;
+        List<Estado> accesibles = new ArrayList<Estado>();
+        return accesibles;
     }
 
 }
