4452fb8a1028d108458bd87f004d39548f848b38
AbelBatalla <110605498+BatallaLko@users.noreply.github.com>
2022-10-19 16:31:41 +0200

    Add files via upload

--- src/CentralEnergia/Estado.java
+++ src/CentralEnergia/Estado.java
@@ -1,11 +1,14 @@
 package CentralEnergia;
 
+import CentralEnergia.Heuristica.*;
 import IA.Energia.Central;
 import IA.Energia.Centrales;
 import IA.Energia.Cliente;
 import IA.Energia.Clientes;
 
 import static CentralEnergia.Utils.Utils.*;
+import static IA.Energia.Cliente.GARANTIZADO;
+
 import CentralEnergia.Heuristica.MaximizarBeneficio;
 import CentralEnergia.Generadores.GeneradorEstadoInicial;
 
@@ -57,6 +60,22 @@ public class Estado {
             return false;
         }
         Cliente clienteToDesasignar = clientes.get(idxCliente);
+        if (clienteToDesasignar.getContrato() == GARANTIZADO) return false;
+        int idxCentral = asignacionClientes[idxCliente];
+        Central centralToDesasignar = centrales.get(idxCentral);
+        asignacionClientes[idxCliente] = NO_ASIGNADO;
+        distribucionCentrales[idxCentral] -= produccionNecesariaToClienteFromCentral(clienteToDesasignar,
+                centralToDesasignar);
+        beneficioCentrales[idxCentral] -= beneficioFromClienteToCentral(clienteToDesasignar,
+                centralToDesasignar);
+        return true;
+    }
+
+    public Boolean desasignar_enswap(int idxCliente) {
+        if (asignacionClientes[idxCliente] == NO_ASIGNADO) {
+            return false;
+        }
+        Cliente clienteToDesasignar = clientes.get(idxCliente);
         int idxCentral = asignacionClientes[idxCliente];
         Central centralToDesasignar = centrales.get(idxCentral);
         asignacionClientes[idxCliente] = NO_ASIGNADO;
@@ -82,6 +101,19 @@ public class Estado {
         beneficioCentrales[idxCentral] += beneficioFromClienteToCentral(cliente, central);
         return true;
     }
+
+    public Boolean swapmodern(int idxCliente1, int idxCentralnova) {
+        int idxCentralantiga = asignacionClientes[idxCliente1];
+        if (idxCentralantiga == idxCentralnova) return false;
+
+        Boolean desasignarCliente1 = this.desasignar_enswap(idxCliente1);
+        if (!desasignarCliente1) return false;
+
+        Boolean asignarCliente1 = this.asignar(idxCliente1, idxCentralnova);
+
+        return (asignarCliente1);
+    }
+
 
     public Boolean swap(int idxCliente1, int idxCliente2) {
         int idxCentral1 = asignacionClientes[idxCliente1];
@@ -126,13 +158,17 @@ public class Estado {
 
     public String toString() {
         MaximizarBeneficio calculadoraBeneficio = new MaximizarBeneficio();
+        MinimizarDistancia calculadoraDistancia = new MinimizarDistancia();
+        LaCombiPerfecta calculadoraRating = new LaCombiPerfecta();
         int numServidos = 0;
         for (int asignado : asignacionClientes) {
             if (asignado != NO_ASIGNADO) {
                 numServidos += 1;
             }
         }
-        return "Servidos: " + numServidos;
-        // + "Beneficio = " + -calculadoraBeneficio.getHeuristicValue(this);
+        return "Servidos: " + numServidos
+         + "  Beneficio: " + -calculadoraBeneficio.getHeuristicValue(this)
+                + "  Distancia: " + calculadoraDistancia.getHeuristicValue(this)
+                + "  Rating: " + calculadoraRating.getHeuristicValue(this);
     }
 }
