302e59bb4448a5e20b970cb10e7b04c90e18fcc3
AbelBatalla <110605498+BatallaLko@users.noreply.github.com>
2022-10-19 16:40:56 +0200

    Add files via upload

--- src/CentralEnergia/GeneradoraSucesores/GeneradoraSucesores.java
+++ src/CentralEnergia/GeneradoraSucesores/GeneradoraSucesores.java
@@ -7,6 +7,8 @@ import CentralEnergia.Estado;
 import aima.search.framework.Successor;
 import aima.search.framework.SuccessorFunction;
 
+import static CentralEnergia.Utils.Utils.NO_ASIGNADO;
+
 public class GeneradoraSucesores implements SuccessorFunction {
 
     @Override
@@ -32,16 +34,35 @@ public class GeneradoraSucesores implements SuccessorFunction {
             }
         }
 
+
+        //swap modern
+        for (int idxCliente1 = 0; idxCliente1 < estadoActual.getAsignacionClientes().length; ++idxCliente1) {
+            int[] assig = estadoActual.getAsignacionClientes();
+            if (assig[idxCliente1] != NO_ASIGNADO) {
+                for (int idxCentral = 0; idxCentral < estadoActual.getDistribucionCentrales().length; ++idxCentral) {
+                    Estado nuevoEstado = new Estado(estadoActual);
+                    if (nuevoEstado.swapmodern(idxCliente1, idxCentral)) {
+                        sucesores.add(new Successor(nuevoEstado.toString(), nuevoEstado));
+                    }
+                }
+            }
+        }
+
+
+
+
+        /*
         // Operador swap
         for (int idxCliente1 = 0; idxCliente1 < estadoActual.getAsignacionClientes().length; ++idxCliente1) {
-            for (int idxCliente2 = idxCliente1 + 1; idxCliente2 < estadoActual
-                    .getAsignacionClientes().length; ++idxCliente2) {
+            for (int idxCliente2 = idxCliente1 + 1; idxCliente2 < estadoActual.getAsignacionClientes().length; ++idxCliente2) {
                 Estado nuevoEstado = new Estado(estadoActual);
                 if (nuevoEstado.swap(idxCliente1, idxCliente2)) {
                     sucesores.add(new Successor(nuevoEstado.toString(), nuevoEstado));
                 }
             }
         }
+
+         */
         return sucesores;
     }
 }
